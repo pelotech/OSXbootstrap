0417ff102a8672ad93aa81cdc030b2caa05ae0c7
JB Brown <JB@Mac.local>
2017-01-04 12:37:04 -0800

    add commented out microsoft office

---
+++ packages/0001_homebrew_One_Liners.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Homebrew packages that have no aditional configuration                      #
+###############################################################################
+
+
+#essentials that have homebrew packages
+brew cask install insync          #mount & sync multiple google drive accounts
+brew cask install google-drive
+brew cask install slack
+brew cask install zoomus          #video conference
+
+
+brew install node
+brew install wget
+brew cask install caffeine        #keep mac awake
+brew cask install dropbox
+brew cask install screenhero      #remote pair programming
+brew cask install skype
+brew cask install macdown         #mark down editor
+brew cask install intellij-idea   #java ide
+#brew cask install microsoft-office
