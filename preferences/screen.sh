392b739075e6171e8046a7402814abec61c7f8ee
jb-brown <jb@controlstatements.com>
2017-01-12 10:28:15 -0800

    add back hot corners

--- preferences/screen.sh
+++ preferences/screen.sh
@@ -21,14 +21,18 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 # 11: Launchpad
 # 12: Notification Center
 
-# Top left screen corner → Mission Control
-#defaults write com.apple.dock wvous-tl-corner -int 2
-#defaults write com.apple.dock wvous-tl-modifier -int 0
+# Top left screen corner → no-op
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tl-modifier -int 0
 
-# Bottom left screen corner → Desktop
-#defaults write com.apple.dock wvous-tr-corner -int 5
-#defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left screen corner → no-op
+defaults write com.apple.dock wvous-bl-corner -int 0
+defaults write com.apple.dock wvous-bl-modifier -int 0
 
 # Top right screen corner → Start screen saver
-#defaults write com.apple.dock wvous-bl-corner -int 4
-#defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock wvous-tr-corner -int 5
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+# Bottom right screen corner → Desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+defaults write com.apple.dock wvous-br-modifier -int 0
